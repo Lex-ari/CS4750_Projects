fd9786ed2fccd7eee73a2a88f28959dbfe01cd70
Lex-ari <amarianocello@gmail.com>
2023-10-03 00:32:49 -0700

    17 Finish

--- CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeDetailFragment.kt
+++ CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeDetailFragment.kt
@@ -11,6 +11,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.FileProvider
+import androidx.core.view.doOnLayout
 import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.setFragmentResultListener
@@ -22,6 +24,7 @@ import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.bignerdranch.android.criminalintent.databinding.FragmentCrimeDetailBinding
 import kotlinx.coroutines.launch
+import java.io.File
 import java.util.Date
 
 
@@ -41,6 +44,17 @@ class CrimeDetailFragment : Fragment() {
     ) {uri: Uri? ->
         uri?.let { parseContactSelection(it)}
     }
+
+    private val takePhoto = registerForActivityResult (
+        ActivityResultContracts.TakePicture()
+    ) { didTakePhoto: Boolean ->
+        if (didTakePhoto && photoName != null){
+            crimeDetailViewModel.updateCrime { oldCrime ->
+                oldCrime.copy(photoFileName = photoName)
+            }
+        }
+    }
+    private var photoName: String? = null
 
     private var _binding: FragmentCrimeDetailBinding? = null
     private val binding
@@ -77,11 +91,30 @@ class CrimeDetailFragment : Fragment() {
                 selectSuspect.launch(null)
             }
 
+            //Checks if user enabled contacts
             val selectSuspectIntent = selectSuspect.contract.createIntent(
                 requireContext(),
                 null
             )
             crimeSuspect.isEnabled = canResolveIntent(selectSuspectIntent)
+
+            crimeCamera.setOnClickListener {
+                photoName = "IMAGE_$(Date()).JPG"
+                val photoFile = File(requireContext().applicationContext.filesDir, photoName)
+                val photoUri = FileProvider.getUriForFile(
+                    requireContext(),
+                    "com.bignerdranch.android.criminalintent.fileprovider",
+                    photoFile,
+                )
+                takePhoto.launch(photoUri)
+            }
+
+            //Used to see if there is a camera app
+            val captureImageIntent = takePhoto.contract.createIntent(
+                requireContext(),
+                null,
+            )
+            crimeCamera.isEnabled = canResolveIntent(captureImageIntent)
         }
 
         viewLifecycleOwner.lifecycleScope.launch {
@@ -139,6 +172,8 @@ class CrimeDetailFragment : Fragment() {
             crimeSuspect.text = crime.suspect.ifEmpty {
                 getString(R.string.crime_suspect_text)
             }
+
+            updatePhoto(crime.photoFileName)
         }
     }
 
@@ -182,6 +217,7 @@ class CrimeDetailFragment : Fragment() {
         }
     }
 
+    //Used to see intent is possible
     private fun canResolveIntent(intent: Intent): Boolean {
         //intent.addCategory(Intent.CATEGORY_HOME) //Temp fix to prevent contact applications to match intent
         val packageManager: PackageManager = requireActivity().packageManager
@@ -190,5 +226,27 @@ class CrimeDetailFragment : Fragment() {
             PackageManager.MATCH_DEFAULT_ONLY
         )
         return resolvedActivity != null
+    }
+
+    private fun updatePhoto(photoFileName: String?) {
+        if (binding.crimePhoto.tag != photoFileName) {
+            val photoFile = photoFileName?.let {
+                File(requireContext().applicationContext.filesDir, it)
+            }
+            if (photoFile?.exists() == true) {
+                binding.crimePhoto.doOnLayout { measuredView ->
+                    val scaledBitmap = getScaledBitmap (
+                        photoFile.path,
+                        measuredView.width,
+                        measuredView.height,
+                    )
+                    binding.crimePhoto.setImageBitmap(scaledBitmap)
+                    binding.crimePhoto.tag = photoFileName
+                }
+            } else {
+                binding.crimePhoto.setImageBitmap(null)
+                binding.crimePhoto.tag = null
+            }
+        }
     }
 }
---
+++ CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/PictureUtils.kt
@@ -0,0 +1,29 @@
+package com.bignerdranch.android.criminalintent
+
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import kotlin.math.roundToInt
+
+fun getScaledBitmap(path: String, destWidth: Int, destHeight: Int): Bitmap {
+
+    // Dimensions of photo in disk
+    val options = BitmapFactory.Options()
+    options.inJustDecodeBounds = true
+    BitmapFactory.decodeFile(path, options)
+    val srcWidth = options.outWidth.toFloat()
+    val srcHeight = options.outHeight.toFloat()
+
+    // Scale photo down
+    val sampleSize = if (srcHeight <= destHeight && srcWidth <= destWidth) {
+        1
+    } else {
+        val heightScale = srcHeight / destHeight
+        val widthScale = srcWidth / destWidth
+        minOf(heightScale, widthScale).roundToInt()
+    }
+
+    // Read + create final bitmap
+    return BitmapFactory.decodeFile(path, BitmapFactory.Options().apply {
+        inSampleSize = sampleSize
+    })
+}
