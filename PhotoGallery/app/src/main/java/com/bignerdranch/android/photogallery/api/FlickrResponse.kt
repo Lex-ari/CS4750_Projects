1ec65692710ec747d4c8a60bc8dc36e8cb8e5bd9
Lex-ari <amarianocello@gmail.com>
2023-10-22 22:33:33 -0700

    Finished Chapter 20

---
+++ PhotoGallery/app/src/main/java/com/bignerdranch/android/photogallery/api/FlickrResponse.kt
@@ -0,0 +1,7 @@
+package com.bignerdranch.android.photogallery.api
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class FlickrResponse(
+    val photos: PhotoResponse
+)
